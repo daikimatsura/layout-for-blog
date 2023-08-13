53d8a97871e10801d432e10a62f1e852c52d4cd1
d.matsuura <matsura@geekfiid.co.jp>
2023-08-13 10:21:57 +0900

    translate authenticator component

--- src/component/templetes/Layout.tsx
+++ src/component/templetes/Layout.tsx
@@ -17,6 +17,13 @@ import theme from '../../style/theme';
 import SideMenu from './SideMenu';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import { I18n } from 'aws-amplify';
+import { translations } from '@aws-amplify/ui-react';
+import { dict } from '../../locales/auth-ja';
+I18n.putVocabularies(translations);
+I18n.setLanguage('ja');
+
+I18n.putVocabularies({ ...dict });
 
 const drawerWidth: number = 240;
 
---
+++ src/locales/auth-ja.js
@@ -0,0 +1,5 @@
+export const dict = {
+    ja: {
+        'Enter your Username': 'ユーザー名を入力してください'
+    }
+}
